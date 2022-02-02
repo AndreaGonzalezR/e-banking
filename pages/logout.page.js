9a7aace4b137e408a0fbcb7fa229339873ecbca5
AndreaGonzalez <andrea.gonzalez@abstracta.com.uy>
2022-02-02 11:20:16 -0300

    Improvements in the organization of the code

--- pages/logout.page.js
+++ pages/logout.page.js
@@ -2,18 +2,22 @@ import BasePage from '../pages/base.page';
 
 class LogoutPage extends BasePage{
 
+    //page locators:
     get userMenu(){return $('.user-avatar')}
     get logoutOption(){return $('=Logout')}
     get logoutSuccess(){return $('.badge')}
 
+
+    //page actions:
     async logout(){
 
         await this.userMenu.click();
         await this.logoutOption.click();
+
         await expect(this.logoutSuccess).toHaveTextContaining('Success');
 
     }
-    
+
 }
 
 export default new LogoutPage();
